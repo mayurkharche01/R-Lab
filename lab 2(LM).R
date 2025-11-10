920a04cbcb2d064352f4bd6dd97ea918162766a5
Mayur Kharche <mayurkharche10@gmail.com>
2025-11-11 00:16:39 +0530

    Lab Manual
    Practical 2
    Importing and Exploring a Dataset in R

---
+++ lab 2(LM).R
@@ -0,0 +1,23 @@
+library(dplyr)
+
+data("iris")
+head(iris)
+str(iris)
+summary(iris)
+data("mtcars")
+
+head(mtcars)
+str(mtcars)
+
+data("Titanic")
+Titanic
+
+sum(is.na(iris)) 
+colSums(is.na(mtcars)) 
+
+iris %>%
+  group_by(Species) %>%
+  summarise(
+    Avg_Sepal_Length = mean(Sepal.Length),
+    Avg_Petal_Length = mean(Petal.Length)
+  )
