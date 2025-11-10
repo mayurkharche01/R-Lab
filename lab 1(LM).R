c7ad50f5804f21dd408417f586d577ae6b554431
Mayur Kharche <mayurkharche10@gmail.com>
2025-11-11 00:09:16 +0530

    Lab Manual
    Lab 1 
    install RStudio and perform basic operations

---
+++ lab 1(LM).R
@@ -0,0 +1,22 @@
+print("Hello, Welcome to R Programming"
+
+      x <- 25
+      y <-5
+      sum_val <-x + y
+      orof. Tusha
+      diff_val <-x-y
+      prod_val <- x * y
+      div_val <- x / y
+      sum_val
+      diff_val
+      prod_val
+      div_val
+      
+      greater_check <- x > y
+      equal_check <- x == y
+     
+      data("iris") 
+      head(iris)
+      str(iris)
+      summary(iris)
+      
