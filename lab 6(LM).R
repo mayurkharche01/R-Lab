de8a919e213de849832c90e2b760aa8d8614cf9b
Mayur Kharche <mayurkharche10@gmail.com>
2025-11-28 17:52:38 +0530

    lab 6(LM).R

--- lab 6(LM).R
+++ lab 6(LM).R
@@ -10,14 +10,14 @@ summary(iris)
 
 ggplot(iris, aes(x = Sepal.Length)) +
   geom_histogram (bins = 20, fill = "lightblue", color = "black") + 
-labs(title = "Distribution of Sepal Length", x = "Sepal Length", y = "Frequency")
+  labs(title = "Distribution of Sepal Length", x = "Sepal Length", y = "Frequency")
 
 ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, color = Species)) +
   geom_point(size = 3) +
   labs(title = "Sepal Length vs Petal Length")
 
 ggplot(iris, aes(x = Species, y = Sepal.Width, fill = Species))
-geom_boxplot() +
+  geom_boxplot() +
   labs (title = "Boxplot of Sepal Width by Species")
 
 corr_matrix = cor(iris[, 1:4])
@@ -25,3 +25,4 @@ corr_matrix
 ggcorrplot(corr_matrix, lab=TRUE, title="Correlation Matrix Heatmap")
 
 ggpairs(iris[, 1:4])
+
