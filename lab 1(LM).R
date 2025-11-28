4b1ba2c2316995d7dd81b22e9678e614cb2ac7d1
Mayur Kharche <mayurkharche10@gmail.com>
2025-11-28 17:33:38 +0530

    lab 1(LM).R
    Refactor R script for clarity and organization.

--- lab 1(LM).R
+++ lab 1(LM).R
@@ -1,22 +1,27 @@
-print("Hello, Welcome to R Programming"
+# Print a message
+print("Hello, Welcome to R Programming")
+#Assign variables
+x <- 25
+y <-5
+sum_val <- x + y
+diff_val <- x - y
+prod_val <- x * у
+div_val <-x /y
+sum_val
+diff_val
+prod_val
+div_val 
+# Relational operations
+greater_check <- x > y
+equal_check <- x == y
+# Load and explore iris dataset
+data("iris")
+head(iris)
+str(iris)
+summary(iris)
+# load dataset
+# first 6 rows
+# structure of dataset
+# descriptive statistics
 
-      x <- 25
-      y <-5
-      sum_val <-x + y
-      orof. Tusha
-      diff_val <-x-y
-      prod_val <- x * y
-      div_val <- x / y
-      sum_val
-      diff_val
-      prod_val
-      div_val
-      
-      greater_check <- x > y
-      equal_check <- x == y
-     
-      data("iris") 
-      head(iris)
-      str(iris)
-      summary(iris)
       
